3761fdb50e0189ff31a2d8c215aa6c6569e9e6b7
goodanshuman <30090283-goodanshuman@users.noreply.replit.com>
2023-12-06 18:36:31 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Updated App component (App.jsx) with Chatbot
+import './App.css';
+import React from 'react';
+import Timer from './Timer';
+
+export default function App() {
+  return (
+    <main>
+      <Timer/>
+    </main>
+  );
+}
